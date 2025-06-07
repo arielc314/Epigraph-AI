7702428eaeecb86f04ccf435e4dd2069a57123fc
Ariel Cohen <arielcohen@Ariels-MacBook-Air.local>
2025-06-07 21:54:51 +0300

    update config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
+            ? '${process.env.NEXT_PUBLIC_API_BASE}/api/:path*'
             : '/api/',
       },
     ]
